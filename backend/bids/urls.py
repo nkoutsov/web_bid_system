ad9eac939cec2feefb40acbc1c4db27d44aa3ba2
JohnKatsa <johnkatsa66@gmail.com>
2019-08-31 20:27:47 +0300

    Test pending for bullets 7,8

--- backend/bids/urls.py
+++ backend/bids/urls.py
@@ -4,5 +4,4 @@ from bids import views
 urlpatterns = [
     path('bids/', views.AuctionList.as_view()),
     path('bids/<int:pk>/', views.AuctionDetail.as_view()),
-    path('authbids/', views.AuctionListAuthorized.as_view()),
 ]
