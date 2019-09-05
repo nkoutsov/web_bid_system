58f70c00e18c96f4b03935ea462da0378509fb6e
Nikos <nikotheract11@gmail.com>
2019-09-05 19:27:17 +0300

    JWT authentication

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from bids import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -29,4 +34,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('',include('bids.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
