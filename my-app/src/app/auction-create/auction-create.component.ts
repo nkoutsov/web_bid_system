e8871cef3e67f0701fe1bd650857e842a715828a
JohnKatsa <johnkatsa66@gmail.com>
2019-09-12 20:46:02 +0300

    erased merge-conflict comments

--- my-app/src/app/auction-create/auction-create.component.ts
+++ my-app/src/app/auction-create/auction-create.component.ts
@@ -1,25 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Auction } from '../models/auction';
 import { AuctionService } from '../auction.service';
+import { Auction } from '../models/auction';
 
 @Component({
-  selector: 'my-app-auction-create',
+  selector: 'app-auction-create',
   templateUrl: './auction-create.component.html',
   styleUrls: ['./auction-create.component.css']
 })
 export class AuctionCreateComponent implements OnInit {
-<<<<<<< HEAD
-  model: any = {};
-
-  map: any;
-  auction : Auction;
-  returnUrl: string;
-
-  constructor(private auctionService : AuctionService) { }
-
-  ngOnInit() {
-  }
-=======
     model: any = {};
   
     map: any;
@@ -59,25 +47,7 @@ export class AuctionCreateComponent implements OnInit {
   
       this.auctionService.postAuction(this.auction).subscribe(data => console.log(data));
     }
->>>>>>> origin/authentication
 
-  submitAuction() {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
-    let auction : Auction = {
-      id: null,
-      name: this.model.name,
-      category: this.model.category,
-      currently: this.model.currently,
-      buy_price: this.model.buy_price,
-      first_bid: this.model.first_bid,
-      number_of_bids: this.model.number_of_bids,
-      location: this.model.location,
-      country: this.model.country,
-      started: this.model.started,
-      ends: this.model.ends,
-      description: this.model.description
-    };
-    
-    this.auctionService.postAuction(auction).subscribe(data => console.log(data));
-  }
+  
 
 }
