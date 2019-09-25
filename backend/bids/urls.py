c00c28901aef1c5165c6e4373d04371360514781
JohnKatsa <johnkatsa66@gmail.com>
2019-09-26 01:09:43 +0300

    export ready

--- backend/bids/urls.py
+++ backend/bids/urls.py
@@ -11,7 +11,7 @@ router.register(r'cats', views.CategoryViewSet)
 router.register(r'inactive',views.PendingUsersViewSet)
 router.register(r'active',views.ActiveUsersViewSet)
 router.register(r'myauctions', views.AuctionViewSet)
-router.register(r'export', views.AdminAuctionViewSet)
+#router.register(r'export', views.AdminAuctionViewSet)
 router.register(r'reco', views.RecommendationViewSet)
 
 urlpatterns = [
